f65132f4538e5f170b3e45c20d8acc6caaa82001
LuizGrochevski <luizfelipegrochevski@gmail.com>
2022-07-21 18:41:09 -0300

    [Programador BR] - Desenvolvimento Web - JS Avançado - Canvas

--- avançado/script.js
+++ avançado/script.js
@@ -31,3 +31,70 @@ ctx.lineWidth = 5;
 ctx.strokeStyle = '#FF0000';
 ctx.stroke();
 
+ctx.rect(10, 10, 100, 200);
+ctx.fillstyle = "blue";
+ctx.fillRect(10, 10, 100, 200);
+ctx.lineWidth = 5;
+ctx.strokeStyle = "red";
+ctx.strokeRect(10, 10, 100, 200);
+
+ctx.fill();
+ctx.stroke();
+
+ctx.clearRect(20, 20, 30, 30);
+
+ctx.beginPath();
+ctx.lineWidth = 4;
+ctx.strokeStyle = "yellow";
+ctx.moveTo(10, 10);
+ctx.lineTo(400, 300);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.lineWidth = 7;
+ctx.strokeStyle = "cyan";
+ctx.fillStyle = "green";
+ctx.moveTo(50,10);
+ctx.lineTo(300, 300);
+ctx.lineTo(400, 300);
+ctx.closePath();
+ctx.fill();
+ctx.stroke();
+
+
+
+let circle = {
+    x : 250,
+    y : 250,
+    raio : 100,
+    inicio : 0,
+    fim : 0,
+    antiHor : true
+}
+
+function drawCircle(circle){
+    /*ctx.beginPath();
+    ctx.Rect(0, 0, 500, 500);
+    ctx.fillStyle = "beige";
+    ctx.fill();*/
+
+    ctx.beginPath();
+    ctx.strokeStyle = "lime";
+    ctx.fillStyle = "purple"
+    ctx.arc(circle.x, circle.y, circle.raio, circle.inicio, circle.fim, circle.antiHor);
+
+    ctx.fill();
+    ctx.stroke();
+}
+
+setInterval(function(){
+     
+    if(circle.fim < 2*Math.PI){
+        circle.fim += 0.3;
+        circle.x += 5;
+    }
+
+    drawCircle(circle);
+
+
+}, 40)
